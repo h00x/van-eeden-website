84a163d4b473bb595625498545df92df11320f24
Dave Hoeks <dave.hoeks@gmail.com>
2018-12-19 19:37:40 +0100

    Trying to figure out what is going wrong with gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,8 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === 'MarkdownRemark') {
+    console.log(node.frontmatter.image.relpath)
+    console.log(node.frontmatter.image.image)
     if (node.frontmatter.image.relpath && node.frontmatter.image.image) {
       const imagepath =
         node.frontmatter.image.relpath + node.frontmatter.image.image
