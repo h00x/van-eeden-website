f4d44be206ca288a8f28cd2413a5acc3b04bf6ef
Dave Hoeks <dave.hoeks@gmail.com>
2018-12-19 19:50:23 +0100

    Change console log

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
 
   if (node.internal.type === 'MarkdownRemark') {
-    console.log(node)
+    console.log(node.fileAbsolutePath)
     console.log(node.frontmatter.image.relpath)
     console.log(node.frontmatter.image.image)
     if (node.frontmatter.image.relpath && node.frontmatter.image.image) {
