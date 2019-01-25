5a70b6b3b07876714c0d5a424f3b28868bc004fd
Dave Hoeks <dave.hoeks@gmail.com>
2019-01-25 13:42:56 +0100

    Styled the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,59 @@
 import React from 'react'
+import { graphql, StaticQuery, Link } from 'gatsby'
+import styled from 'styled-components'
+import Img from 'gatsby-image'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  const ContentWrapper = styled.div`
+    text-align: center;
+  `
+
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          logo: file(relativePath: { eq: "van-eeden-logo.png" }) {
+            childImageSharp {
+              fixed(width: 229) {
+                ...GatsbyImageSharpFixed_noBase64
+              }
+            }
+          }
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={data => (
+        <Layout>
+          <SEO
+            title="Pagina bestaat niet"
+            keywords={[
+              'hyacinten',
+              'kweekers',
+              'bollenstreek',
+              'noordwijkerhout',
+              'van eeden',
+              'hyacint met bol',
+            ]}
+          />
+          <ContentWrapper>
+            <Img
+              fixed={data.logo.childImageSharp.fixed}
+              alt={data.site.siteMetadata.title}
+            />
+            <h1>404 Pagina niet gevonden.</h1>
+            <Link to="/">Ga naar de Homepage</Link>
+          </ContentWrapper>
+        </Layout>
+      )}
+    />
+  )
+}
 
 export default NotFoundPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 
+import Header from '../components/header'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import HeaderImage from '../components/header-image'
@@ -12,25 +14,39 @@ import '../../node_modules/slick-carousel/slick/slick.css'
 import '../../node_modules/slick-carousel/slick/slick-theme.css'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO
-      title="Home"
-      keywords={[
-        'hyacinten',
-        'kweekers',
-        'bollenstreek',
-        'noordwijkerhout',
-        'van eeden',
-        'hyacint met bol',
-      ]}
-    />
-    <HeaderImage />
-    <FirstItem />
-    <ImageGallery />
-    <SecondItem />
-    <ContactBlock />
-    <Footer />
-  </Layout>
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Layout>
+        <SEO
+          title="Home"
+          keywords={[
+            'hyacinten',
+            'kweekers',
+            'bollenstreek',
+            'noordwijkerhout',
+            'van eeden',
+            'hyacint met bol',
+          ]}
+        />
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <HeaderImage />
+        <FirstItem />
+        <ImageGallery />
+        <SecondItem />
+        <ContactBlock />
+        <Footer />
+      </Layout>
+    )}
+  />
 )
 
 export default IndexPage
